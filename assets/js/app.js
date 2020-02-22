8c8e24d55dab37584f2ffd5373f9d0e6dcce209f
Cedric <c.caudron59@gmail.com>
2020-02-22 17:48:52 +0100

    Link React to Symfony ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,24 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
 import '../css/app.css';
+import ReactDOM from "react-dom";
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+
+
+const App = () => {
+    return (
+        <h1>
+        Salut
+        </h1>
+)
+    ;
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App/>, rootElement);
+
+
+
+
+
 
 
