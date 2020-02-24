e6e28810af75898c4016bc14e0d35f54109b38f1
Cedric <c.caudron59@gmail.com>
2020-02-24 16:06:39 +0100

    configure NavbarTop

---
+++ assets/js/components/navbars/NavbarTop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarTop = ({history}) => {
+    return (
+        <div>
+            <h1>NavbarTop</h1>
+        </div>
+    );
+};
+
+export default NavbarTop;
