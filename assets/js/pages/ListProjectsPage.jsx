5710c78d21e449a4c49b680c6b8fa09b41686595
Cedric <c.caudron59@gmail.com>
2020-02-26 21:12:14 +0100

    Adding style to list projects page
    Create enums

--- assets/js/pages/ListProjectsPage.jsx
+++ assets/js/pages/ListProjectsPage.jsx
@@ -10,6 +10,20 @@ const ListProjectsPage = (props) => {
     const [projects, setProjects] = useState([]);
     const {searchValue} = useContext(SearchContext);
 
+    const STATUS_CLASSES = {
+        no_start: "info",
+        in_progress: "warning",
+        finished: "success",
+        archived: "primary"
+    };
+
+    const STATUS_LABEL = {
+        no_start: "Pas démarré",
+        in_progress: "En cours",
+        finished: "Fini",
+        archived: "Archivé"
+    };
+
     useEffect(() => {
         setProjects(
             // Création d'un faux tableau pour les tests
@@ -20,66 +34,66 @@ const ListProjectsPage = (props) => {
                     name: "Ma maison",
                     description: "On peut y faire quelques travaux??",
                     photo: "../img/projects-img/projects-general-img/1-project-img.jpg",
-                    statut: "Pas démarré",
-                    adresse1 : "346 rue de Surprise",
-                    adresse2 : "",
-                    code_postal : "59000",
-                    ville : "Bonne-Tranche-Sur-Mer"
+                    statut: "no_start",
+                    adresse1: "346 rue de Surprise",
+                    adresse2: "",
+                    code_postal: "59000",
+                    ville: "Bonne-Tranche-Sur-Mer"
                 },
                 {
                     id: 2,
                     name: "Mon jardin",
                     description: "Un petit nettoyage serait pas de refus :-)",
                     photo: "../img/projects-img/projects-general-img/2-project-img.jpg",
-                    statut: "En cours",
-                    adresse1 : "346 rue de Surprise",
-                    adresse2 : "",
-                    code_postal : "59000",
-                    ville : "Bonne-Tranche-Sur-Mer"
+                    statut: "in_progress",
+                    adresse1: "346 rue de Surprise",
+                    adresse2: "",
+                    code_postal: "59000",
+                    ville: "Bonne-Tranche-Sur-Mer"
                 },
                 {
                     id: 3,
                     name: "Le radôme",
                     description: "Un super musée",
                     photo: "../img/projects-img/projects-general-img/3-project-img.jpg",
-                    statut: "Fini",
-                    adresse1 : "Le Radôme",
-                    adresse2 : "Cité des Télécoms",
-                    code_postal : "22560",
-                    ville : "Pleumeur-Bodou"
+                    statut: "finished",
+                    adresse1: "Le Radôme",
+                    adresse2: "Cité des Télécoms",
+                    code_postal: "22560",
+                    ville: "Pleumeur-Bodou"
                 },
                 {
                     id: 4,
                     name: "Fort-Mon-Chateau",
                     description: "A l'assault Jacqouille!!!!",
                     photo: "../img/projects-img/projects-general-img/4-project-img.jpg",
-                    statut: "En cours",
-                    adresse1 : "27 rue des Sarrazins",
-                    adresse2 : "",
-                    code_postal : "15000",
-                    ville : "Montmirail"
+                    statut: "in_progress",
+                    adresse1: "27 rue des Sarrazins",
+                    adresse2: "",
+                    code_postal: "15000",
+                    ville: "Montmirail"
                 },
                 {
                     id: 5,
                     name: "Belle-Qui-Dort",
                     description: "Un jour mon prince viendra.....",
                     photo: "../img/projects-img/projects-general-img/5-project-img.jpg",
-                    statut: "Archivé",
-                    adresse1 : "666 allée des somnifères",
-                    adresse2 : "Entrée B",
-                    code_postal : "00000",
-                    ville : "Dodo-sur-Isère"
+                    statut: "archived",
+                    adresse1: "666 allée des somnifères",
+                    adresse2: "Entrée B",
+                    code_postal: "00000",
+                    ville: "Dodo-sur-Isère"
                 },
                 {
                     id: 6,
                     name: "Empire State Building",
                     description: "C'est haut mon capitaine!",
                     photo: "../img/projects-img/projects-general-img/6-project-img.jpg",
-                    statut: "En cours",
-                    adresse1 : "20 W 34th Street",
-                    adresse2 : "",
-                    code_postal : "10001",
-                    ville : "New-York"
+                    statut: "in_progress",
+                    adresse1: "20 W 34th Street",
+                    adresse2: "",
+                    code_postal: "10001",
+                    ville: "New-York"
                 }
             ]
         );
@@ -89,7 +103,6 @@ const ListProjectsPage = (props) => {
     const filteredProjects = projects.filter(
         p =>
             p.name.toLowerCase().includes(searchValue.toLowerCase())
-
     );
 
 
@@ -102,17 +115,24 @@ const ListProjectsPage = (props) => {
                 {filteredProjects.map(project =>
                     <div key={project.id}>
 
-                        <div className="card m-4" style={{width: '20rem', height: '24rem'}}>
+                        <div className="card m-4" style={{width: '20rem', height: '26rem'}}>
+
+                            <h5 className="card-title p-2">{project.name}</h5>
+
                             <ImgWithStyleComponent
-                                className="card-img-top p-1"
+                                className="card-img-top"
                                 src={project.photo}
                                 alt={project.name}
                                 style={{height: "10rem"}}
                             />
                             <div className="card-body">
-                                <h5 className="card-title">{project.name}</h5>
-                                <p className="card-text">Description: {project.description}</p>
+                                <p className="card-text text-right mb-3">{project.ville}</p>
+                                <p className="card-text mb-1">Description:</p>
+                                <p className="font-weight-light font-italic card-text">{project.description}</p>
 
+                            </div>
+                            <div className="card-footer pb-0 text-right">
+                                <p><span className={"pl-2 pr-2 pt-1 pb-1 badge badge-" + STATUS_CLASSES[project.statut]}>{STATUS_LABEL[project.statut]}</span></p>
                             </div>
                         </div>
                     </div>
