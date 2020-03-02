52a5d075e912e2b82ae37f082140facde7529425
Vincent Brocheton <vinc.brocheton@gmail.com>
2020-03-02 09:59:21 +0100

    Commentaire Installations

--- assets/js/pages/ReportInstallationsPage.jsx
+++ assets/js/pages/ReportInstallationsPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NavbarLeft from "../components/navbars/NavbarLeft";
+import '../../css/report.css'
+import {withRouter} from "react-router-dom";
+import Button from "../components/forms/Button";
+import Field from "../components/forms/Field";
 
 const ReportInstallationsPage = () => {
-    return (
-        <div>
 
-        </div>
+    const NavbarLeftWithRouter = withRouter(NavbarLeft);
+
+    const [commentaire, setCommentaire] = useState("");
+
+    //Gestion des changement d'input dans le form
+    const handleChange = ({currentTarget}) => {
+        setCommentaire(currentTarget.value);
+    };
+
+    return (
+        <>
+            <NavbarLeftWithRouter/>
+
+            <Field placeholder="Commentaire des installations du chantier" onChange={handleChange}
+                   label="Installations de chantier" name="installation" value={commentaire}/>
+            <Button className="btn btn-info float-right" type="submit" text="Envoyer"/>
+        </>
     );
 };
 
