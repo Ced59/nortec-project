419c24fc319b0413b76110c75078f588a8e1213e
Vincent Brocheton <vinc.brocheton@gmail.com>
2020-03-04 11:55:18 +0100

    Page de Securité

--- assets/js/pages/ReportSecuritePage.jsx
+++ assets/js/pages/ReportSecuritePage.jsx
@@ -1,25 +1,73 @@
-import React, {useContext, useEffect} from 'react';
-import SelectedNavBarLeftContext from "../contexts/SelectedNavBarLeftContext";
+import React, {useState} from 'react';
 import {withRouter} from "react-router-dom";
 import NavbarLeft from "../components/navbars/NavbarLeft";
+import Button from "../components/forms/Button";
+import FieldTextArea from "../components/forms/FieldTextArea";
+import ImageUpload from "../components/forms/ImageUpload";
+import Select from "../components/forms/Select";
 
 const ReportSecuritePage = () => {
 
-
-    const {setSelectedValue} = useContext(SelectedNavBarLeftContext);
-
     const NavbarLeftWithRouter = withRouter(NavbarLeft);
 
-    useEffect(() => {
-        setSelectedValue('securite');
-    });
+    const [conforme, setConforme] = useState(null) ;
+
+    const handleCheckConforme = () => {
+        if(!conforme || conforme === false) {
+            setConforme(true);
+        }
+    };
+
+    const handleCheckNonConforme = () => {
+        if(conforme || conforme === null){
+            setConforme(false);
+        }
+    };
+
+    const handleSubmit = () => {
+
+    };
 
     return (
         <>
             <NavbarLeftWithRouter/>
 
             <div className='page-content'>
-                <p>Sécurité</p>
+                <p>Propreté des accès :</p>
+                <Button onClick={handleCheckConforme} className="btn btn-success" text="Conforme" type="button"/>
+                <Button onClick={handleCheckNonConforme} className="btn btn-danger ml-2" text="Non Conforme" type="button"/>
+            </div>
+            {(conforme &&
+                <div className='page-content'>
+                    <p>Ceci est conforme</p>
+                </div>
+            )};
+            {(conforme === false &&
+                <div className='page-content'>
+                    <div className="row">
+                        <div>
+                            <Select label="Entreprise en charge" >
+                                <option>Entreprise A</option>
+                                <option>Entreprise B</option>
+                                <option>Entreprise C</option>
+                            </Select>
+                        </div>
+                        <div className="ml-auto">
+                            <ImageUpload buttonText="Choisir l'image"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <FieldTextArea label="Commentaire : " placeholder="Commentaire pour toute les entreprises" />
+                        </div>
+                        <div className="col-6">
+                            <FieldTextArea label="Commentaire interne : " placeholder="Commentaire pour toute les entreprises" />
+                        </div>
+                    </div>
+                </div>
+            )};
+            <div className='page-content'>
+                <Button onClick={handleSubmit} className="btn btn-primary" text="Valider" type="submit" />
             </div>
 
         </>
