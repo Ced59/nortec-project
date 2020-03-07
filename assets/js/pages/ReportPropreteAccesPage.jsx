b3f942ed1ce34d81a991f28d3414223fdb3ce1db
Cedric <c.caudron59@gmail.com>
2020-03-07 18:30:53 +0100

    adding prorata, editing comments

--- assets/js/pages/ReportPropreteAccesPage.jsx
+++ assets/js/pages/ReportPropreteAccesPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {withRouter} from "react-router-dom";
 import NavbarLeft from "../components/navbars/NavbarLeft";
 import Button from "../components/forms/Button";
@@ -11,7 +11,10 @@ import {toast} from "react-toastify";
 
 const ReportPropreteAccesPage = ({match}) => {
 
-    const [conforme, setConforme] = useState(null);
+    const [conforme, setConforme] = useState("");
+    const [report, setReport] = useState("");
+    const [comment, setComment] = useState("");
+    const [commentIntern, setCommentIntern] = useState("");
 
     const NavbarLeftWithRouter = withRouter(NavbarLeft);
 
@@ -19,15 +22,40 @@ const ReportPropreteAccesPage = ({match}) => {
     const reportById = fakeData.reportById(parseInt(urlParams.idReport, 10));
 
 
+    const fetchReport = () => {
+
+        //Vérification si édition ou nouveau rapport... Dans la version finale, le nouveau rapport existera mais avec valeurs vides donc pas de vérification à ce niveau
+        if (reportById) {
+            setReport(reportById);
+            setConforme(reportById.proprete_access_conformity);
+            setComment(reportById.proprete_access_comment);
+            setCommentIntern(reportById.proprete_access_comment_intern);
+        }
+
+    };
+
+    useEffect(() => {
+
+        fetchReport();
+
+    }, []);
+
+
     const handleCheckConforme = () => {
-        if (!conforme || conforme === false) {
-            setConforme(true);
+        if (conforme !== "conform") {
+            setConforme("conform");
+        }
+    };
+
+    const handleCheckProrata = () => {
+        if (conforme !== "prorata") {
+            setConforme("prorata");
         }
     };
 
     const handleCheckNonConforme = () => {
-        if (conforme || conforme === null) {
-            setConforme(false);
+        if (conforme !== "noconform") {
+            setConforme("noconform");
         }
     };
 
@@ -36,9 +64,16 @@ const ReportPropreteAccesPage = ({match}) => {
     };
 
     const handleSubmitConform = () => {
-        //TODO enregistrement de la conformité à true
+        //TODO enregistrement de la conformité à conform
         toast.success("Statut de la propreté des accès enregistré avec succès!")
     };
+
+    const handleSubmitProrata = () => {
+        //TODO enregistrement de la conformité à prorata
+        toast.success("Statut de la propreté des accès enregistré avec succès!")
+    };
+
+
 
 
     return (
@@ -50,11 +85,13 @@ const ReportPropreteAccesPage = ({match}) => {
                     <h2 className="mb-4">Propreté des accès :</h2>
                     <Button onClick={handleCheckConforme} className="btn btn-success mb-4" text="Conforme"
                             type="button"/>
+                    <Button onClick={handleCheckProrata} className="btn btn-warning ml-5 mb-4" text="Prorata"
+                            type="button"/>
                     <Button onClick={handleCheckNonConforme} className="btn btn-danger ml-5 mb-4" text="Non Conforme"
                             type="button"/>
                 </div>
 
-                {(conforme &&
+                {(conforme === "conform" &&
 
                     <div className='card mt-3'>
                         <div className='row ml-2 d-flex justify-content-center mt-3'>
@@ -67,7 +104,20 @@ const ReportPropreteAccesPage = ({match}) => {
                     </div>
 
                 )}
-                {(conforme === false &&
+                {(conforme === "prorata" &&
+
+                    <div className='card mt-3'>
+                        <div className='row ml-2 d-flex justify-content-center mt-3'>
+                            <h4 className='mb-4'>Propreté des accès au prorata ?</h4>
+                        </div>
+                        <div className='row ml-2 d-flex justify-content-center'>
+                            <Button onClick={handleSubmitProrata} className="btn btn-info mb-4 row" text="Valider"
+                                    type="button"/>
+                        </div>
+                    </div>
+
+                )}
+                {(conforme === "noconform" &&
 
                     <>
                         <div className="row">
@@ -89,11 +139,15 @@ const ReportPropreteAccesPage = ({match}) => {
                         <div className="row">
                             <div className="col-6">
                                 <FieldTextArea label="Commentaire : "
-                                               placeholder="Commentaire pour toute les entreprises"/>
+                                               placeholder="Commentaire pour toute les entreprises"
+                                               value={comment}
+                                />
                             </div>
                             <div className="col-6">
                                 <FieldTextArea label="Commentaire interne : "
-                                               placeholder="Commentaire pour toute les entreprises"/>
+                                               placeholder="Commentaire pour toute les entreprises"
+                                               value={commentIntern}
+                                />
                             </div>
                         </div>
                     </>
