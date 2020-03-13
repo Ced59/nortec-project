471051cb379c365c7a45294155a28a2c8555a24e
Cedric <c.caudron59@gmail.com>
2020-03-13 11:24:51 +0100

    commit minor changes

--- assets/js/pages/ReportValidatePage.jsx
+++ assets/js/pages/ReportValidatePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Button from "../components/forms/Button";
-import FieldTextArea from "../components/forms/FieldTextArea";
-import ImageUpload from "../components/forms/ImageUpload";
 import {withRouter} from "react-router-dom";
 import NavbarLeft from "../components/navbars/NavbarLeft";
 
