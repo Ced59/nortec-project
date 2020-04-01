fc00a2bb7ac8a44006842c085179518c166ea080
Vincent Brocheton <vinc.brocheton@gmail.com>
2020-04-01 14:14:34 +0200

    Page Admin Button

--- assets/js/pages/AdminPage.jsx
+++ assets/js/pages/AdminPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
+import { Card, Button, CardTitle, CardImg } from 'reactstrap';
+
 const AdminPage = ({history}) => {
 
     return <main className="container">
-        <NavLink className="btn btn-primary" to="/admin/project"> Projet </NavLink>
-        <NavLink className="btn btn-secondary" to="/admin/userslist"> Utilisateur </NavLink>
+        <div className="row container">
+        <Card className="col-4 mt-5" body inverse color="info">
+            <CardTitle>Projet</CardTitle>
+            <CardImg></CardImg>
+            <NavLink className="btn btn-primary" to="/admin/project"> Projet </NavLink>
+        </Card>
+        <Card className="col-4 mt-5" body inverse color="info">
+            <CardTitle>Utilisateur</CardTitle>
+            <CardImg></CardImg>
+            <NavLink className="btn btn-secondary" to="/admin/userslist"> Utilisateur </NavLink>
+        </Card>
+        </div>
+
     </main>
 
 };
