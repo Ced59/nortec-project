79d01830b11a9139287b9402f82701595ead4792
Cedric <c.caudron59@gmail.com>
2020-06-05 15:49:49 +0200

    adding Entities Project and ProjectDateFinPrevue

---
+++ src/Entity/ProjectDateFinPrevue.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjectDateFinPrevueRepository")
+ */
+class ProjectDateFinPrevue
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Project", inversedBy="date_fin_prevues")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Project;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getProject(): ?Project
+    {
+        return $this->Project;
+    }
+
+    public function setProject(?Project $Project): self
+    {
+        $this->Project = $Project;
+
+        return $this;
+    }
+}
