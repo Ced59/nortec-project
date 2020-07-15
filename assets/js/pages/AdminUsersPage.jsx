a79260b31d903fa56ce018f0ef95a7de2f5363e3
Vincent Brocheton <vinc.brocheton@gmail.com>
2020-07-15 22:15:03 +0200

    pagination User

--- assets/js/pages/AdminUsersPage.jsx
+++ assets/js/pages/AdminUsersPage.jsx
@@ -1,14 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const AdminUsersPage = ({history}) => {
 
     const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         Axios.get("http://localhost:8000/api/users").then(response => response.data['hydra:member']).then(data => setUsers(data));
     }, []);
+
+    const handleDelete = async id => {
+
+        const originalUsers = [...users];
+
+        setUsers(users.filter(user => user.id !== id));
+
+        try {
+            await Axios.delete("http://localhost:8000/api/users/" + id);
+            toast.success("L'utilisateur a bien été supprimé !");
+        } catch (error) {
+            setUsers(originalUsers);
+            toast.error("L'utilisateur n'a pas été correctement supprimé !");
+        }
+        
+    }
+
+    const handleChangePage = page => {
+        setCurrentPage(page);
+    }
+
+    const itemsPerPage = 8;
+    const pagesCount = Math.ceil(users.length / itemsPerPage);
+    const pages = [];
+
+    for(let i=1 ; i<=pagesCount; i++){
+        pages.push(i);
+    }
+
+    const start = currentPage * itemsPerPage - itemsPerPage;
+    const paginatedUsers = users.slice(start, start + itemsPerPage)
+
+    console.log(pages);
 
     return <main className="container">
         <div className="row">
@@ -31,18 +66,38 @@ const AdminUsersPage = ({history}) => {
                 </tr>
             </thead>
             <tbody>
-                {users.map(user =>
+                {paginatedUsers.map(user =>
                 <tr key={user.id}>
                     <td>{user.id}</td>
                     <td>{user.lastName} </td>
                     <td>{user.firstName} </td>
                     <td>{user.email} </td>
                     <td></td>
-                    <td></td>
+                    <td>
+                        <button onClick={() => handleDelete(user.id)} className="btn btn-danger">Supprimer</button>
+                    </td>
                 </tr>
                     )}
             </tbody>
         </table>
+
+        
+        <div>
+  <ul className="pagination pagination-sm">
+    <li className={"page-item" + ( currentPage === 1 && " disabled")}>
+      <button className="page-link" onClick={() => handleChangePage(currentPage - 1)}>&laquo;</button>
+    </li>
+    {pages.map(page => 
+        <li key={page} className={"page-item" + ( currentPage === page && " active")}>
+      <button className="page-link" onClick={() => handleChangePage(page)}>{page}</button>
+    </li>
+        )}
+    
+    <li className={"page-item" + ( currentPage === pagesCount && " disabled")}>
+      <button className="page-link" onClick={() => handleChangePage(currentPage + 1)}>&raquo;</button>
+    </li>
+  </ul>
+</div>
     </main>
 
 };
