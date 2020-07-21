a2cb2229963ee42b5d263e798116bb917e97eab5
Cedric <c.caudron59@gmail.com>
2020-07-21 15:04:00 +0200

    adding loading icon

--- assets/js/pages/AdminUsersPage.jsx
+++ assets/js/pages/AdminUsersPage.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import userApi from '../services/userApi';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import UserApi from '../services/UserApi';
+import '../../css/loading-icon.css';
+import '../../css/pagination-centered.css'
 
 const AdminUsersPage = ({history}) => {
 
     const [users, setUsers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        fetchUsers().then(r => "");
+    }, []);
 
 // --------------------------- Récupérer tout les Utilisateurs ---------------------------
 
-    useEffect(() => {
-        userApi.findAll().then(data => setUsers(data));
-    }, []);
+    const fetchUsers = async () => {
+        try {
+            const data = await UserApi.findAll();
+            setUsers(data);
+            setLoading(false);
+        } catch (error) {
+            toast.error("Erreur lors du chargement de la liste des utilisateurs");
+        }
+    }
 
 //------------------------------Suppression d'un Utilisateur ------------------------------
 
@@ -23,13 +37,13 @@ const AdminUsersPage = ({history}) => {
         setUsers(users.filter(user => user.id !== id));
 
         try {
-            await userApi.deleteUser(id)
+            await UserApi.deleteUser(id)
             toast.success("L'utilisateur a bien été supprimé !");
         } catch (error) {
             setUsers(originalUsers);
             toast.error("L'utilisateur n'a pas été correctement supprimé !");
         }
-        
+
     }
 
 // ----------------------------- Mise en place de la pagination ------------------------------
@@ -42,7 +56,7 @@ const AdminUsersPage = ({history}) => {
     const pagesCount = Math.ceil(users.length / itemsPerPage);
     const pages = [];
 
-    for(let i=1 ; i<=pagesCount; i++){
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
@@ -52,27 +66,28 @@ const AdminUsersPage = ({history}) => {
 
     return <main className="container">
         <div className="mb-4 d-flex justify-content-between align-items-center">
-        <h2> Utilisateurs : </h2>
-        <Link
-            className='btn btn-primary'
-            type='button'
-            to={'/admin/user/new'}
-        > Nouvel Utilisateur </Link>
+            <h2> Utilisateurs : </h2>
+            <Link
+                className='btn btn-primary'
+                type='button'
+                to={'/admin/user/new'}
+            > Nouvel Utilisateur </Link>
         </div>
         <table className="table table-hover">
             <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Email</th>
-                    <th>Rôle</th>
-                    <th></th>
-                    <th></th>
-                </tr>
+            <tr>
+                <th>Id</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Email</th>
+                <th>Rôle</th>
+                <th></th>
+                <th></th>
+            </tr>
             </thead>
+            {!loading &&
             <tbody>
-                {paginatedUsers.map(user =>
+            {paginatedUsers.map(user =>
                 <tr key={user.id}>
                     <td>{user.id}</td>
                     <td>{user.lastName} </td>
@@ -86,27 +101,31 @@ const AdminUsersPage = ({history}) => {
                         <Link to={"/admin/user/" + user.id} className="btn btn-success">Modifier</Link>
                     </td>
                 </tr>
-                    )}
+            )}
             </tbody>
+            }
         </table>
+        {loading &&
+        <div id="loading-icon" class="mt-5 mb-5"></div>
+        }
 
-        
-        <div>
-  <ul className="pagination pagination-sm">
-    <li className={"page-item" + ( currentPage === 1 && " disabled")}>
-      <button className="page-link" onClick={() => handleChangePage(currentPage - 1)}>&laquo;</button>
-    </li>
-    {pages.map(page => 
-        <li key={page} className={"page-item" + ( currentPage === page && " active")}>
-      <button className="page-link" onClick={() => handleChangePage(page)}>{page}</button>
-    </li>
-        )}
-    
-    <li className={"page-item" + ( currentPage === pagesCount && " disabled")}>
-      <button className="page-link" onClick={() => handleChangePage(currentPage + 1)}>&raquo;</button>
-    </li>
-  </ul>
-</div>
+
+        <div class="mt-2">
+            <ul className="pagination pagination-sm justify-content-center">
+                <li className={"page-item" + (currentPage === 1 && " disabled")}>
+                    <button className="page-link" onClick={() => handleChangePage(currentPage - 1)}>&laquo;</button>
+                </li>
+                {pages.map(page =>
+                    <li key={page} className={"page-item" + (currentPage === page && " active")}>
+                        <button className="page-link" onClick={() => handleChangePage(page)}>{page}</button>
+                    </li>
+                )}
+
+                <li className={"page-item" + (currentPage === pagesCount && " disabled")}>
+                    <button className="page-link" onClick={() => handleChangePage(currentPage + 1)}>&raquo;</button>
+                </li>
+            </ul>
+        </div>
     </main>
 
 };
