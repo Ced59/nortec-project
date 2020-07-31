a0a6799ba542312a919c2a7b50c0c457f81bfc59
Cedric <c.caudron59@gmail.com>
2020-07-31 15:20:41 +0200

    change to https links

--- assets/js/components/configs/api_links_config.js
+++ assets/js/components/configs/api_links_config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL = "https://localhost:8000/api/";
 
 export const LOGIN_API = API_URL + "login_check";
 export const USERS_API = API_URL + "users";
