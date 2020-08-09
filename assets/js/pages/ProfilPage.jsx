c9376082b4af8207b3681c4b24bf733570b4583d
Vincent Brocheton <vinc.brocheton@gmail.com>
2020-08-09 11:52:35 +0200

    Change Profil Page

--- assets/js/pages/ProfilPage.jsx
+++ assets/js/pages/ProfilPage.jsx
@@ -7,22 +7,24 @@ const ProfilPage = ({history}) => {
     const [FirstNameUser] = useState(AuthAPI.getUserFirstname());
     const [LastNameUser] = useState(AuthAPI.getUserLastName());
     const [MailUser] = useState(AuthAPI.getUsername());
+    const [edit, setEdit] = useState(false);
 
+    const ChangeMail = () => {
+        setEdit(true);
+    }
 
     return <main className="container">
             <h3>Information du Profil :</h3>
-        <div className="row">
-            <div className="ml-4 mt-4">Prénom : {FirstNameUser} </div>
-            <div className="ml-3 mt-4"> Modifier </div>
-        </div>
-        <div className="row">
-            <div className="ml-4 mt-4">Nom : {LastNameUser}</div>
-            <div className="ml-3 mt-4"> Modifier </div>
-        </div>
-        <div className="row">
-            <div className="ml-4 mt-4">Mail : {MailUser} </div>
-            <div className="ml-3 mt-4"> Modifier </div>
-        </div>
+            <div className="row">
+                <div className="ml-4 mt-4">Prénom : {FirstNameUser} </div>
+            </div>
+            <div className="row">
+                <div className="ml-4 mt-4">Nom : {LastNameUser}</div>
+            </div>
+            <div className="row">
+                <div className="ml-4 mt-4">Mail : {MailUser} </div>
+                <button className="btn btn-primary" onClick={() => ChangeMail()}> Modifier </button>
+            </div>
         </main>
 
 
