75007cbdbd2db9df4dd01a5f549acf519bd57482
Sponts <chqtcharles@gmail.com>
2020-10-26 14:57:33 +0100

    Mise en place de l'onglet échéance

--- assets/js/pages/ReportEcheancesPage.jsx
+++ assets/js/pages/ReportEcheancesPage.jsx
@@ -1,107 +1,338 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import NavbarLeft from "../components/navbars/NavbarLeft";
 import Select from "../components/forms/Select";
 import Button from "../components/forms/Button";
 import "../../css/app.css";
+import Modal from "react-bootstrap/Modal";
+import Field from "../components/forms/Field";
+import DateAPI from "../services/DateAPI";
+import ProjectsAPI from "../services/ProjectsAPI";
+import fakeData from "../components/fakeDataForDev/fakeData";
 
-const ReportEcheancesPage = () => {
+const ReportEcheancesPage = ({ match }) => {
   const NavbarLeftWithRouter = withRouter(NavbarLeft);
 
-  return (
-    <main>
-      <NavbarLeftWithRouter selected="echeances" />
+  const [showModalEcheance, setShowModalEcheance] = useState(false);
+  const [showModalDetail, setShowModalDetail] = useState(false);
+  const [echeance, setEcheance] = useState({
+    numero: "",
+    redacteur: "Charles",
+    status: "",
+    category: "",
+    subject: "",
+    dateDebut: "",
+    dateFin: "",
+    lotId: {
+      id: "",
+      entreprise: {
+        nom: "",
+      },
+    },
+  });
+  const [report, setReport] = useState({});
+  const [edit, setEdit] = useState(false);
 
-      <div className="page-content">
-        <h2 className="mb-4">Echéances : </h2>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Numéro</th>
-              <th>Rédacteur</th>
-              <th>Statut</th>
-              <th>Catégorie</th>
-              <th>Sujet</th>
-              <th>Création</th>
-              <th>Echéance</th>
-              <th>Clotûre</th>
-              <th>Retard</th>
-              <th>En charge</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>4521</td>
-              <td>Florent</td>
-              <td>
-                <Select value="Statut">
-                  <option>Terminé</option>
-                  <option>En cours</option>
-                  <option>Retard</option>
-                  <option>Pas encore démarré</option>
-                </Select>
-              </td>
-              <td>MOQUETTE</td>
-              <td>Pose moquette RDC</td>
-              <td>02/02/2020</td>
-              <td>04/02/2020</td>
-              <td></td>
-              <td>2</td>
-              <td>Salamèche</td>
-              <td>
-                <Button className="btn btn-primary" text="Détails" />
-              </td>
-            </tr>
-            <tr>
-              <td>75234</td>
-              <td>Cedric</td>
-              <td>
-                <Select value="Statut">
-                  <option>Terminé</option>
-                  <option>En cours</option>
-                  <option>Retard</option>
-                  <option>Pas encore démarré</option>
-                </Select>
-              </td>
-              <td>PEINTURE</td>
-              <td>Peinture de la chambre</td>
-              <td>03/12/2019</td>
-              <td>22/12/2019</td>
-              <td>03/01/2020</td>
-              <td>2</td>
-              <td>Bulbizarre</td>
-              <td>
-                <Button className="btn btn-primary" text="Détails" />
-              </td>
-            </tr>
-            <tr>
-              <td>6542</td>
-              <td>Vincent</td>
-              <td>
-                <Select value="Statut">
-                  <option>Terminé</option>
-                  <option>En cours</option>
-                  <option>Retard</option>
-                  <option>Pas encore démarré</option>
-                </Select>
-              </td>
-              <td>PLOMBERIE</td>
-              <td>Plomberie de la salle de bain</td>
-              <td>01/03/2020</td>
-              <td>15/03/2020</td>
-              <td></td>
-              <td></td>
-              <td>Carapuce</td>
-              <td>
-                <Button className="btn btn-primary" text="Détails" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button className="btn btn-primary">Ajouter une échéance</button>
-      </div>
-    </main>
+  const STATUS_CLASSES = {
+    no_start: "info",
+    in_progress: "warning",
+    finished: "success",
+    late: "danger",
+  };
+
+  const STATUS_LABEL = {
+    no_start: "Pas démarré",
+    in_progress: "En cours",
+    finished: "Fini",
+    late: "En retard",
+  };
+
+  const handleShowModalEcheance = () => {
+    setShowModalEcheance(true);
+  };
+  const handleShowModalDetail = (id) => {
+    setShowModalDetail(true);
+    try {
+      const echeanceById = fakeData.echeanceById(id);
+      setEcheance(echeanceById);
+      console.log(echeance);
+      console.log(numlot);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const handleCloseModalEcheance = () => {
+    setShowModalEcheance(false);
+  };
+  const handleCloseModalDetail = () => {
+    setShowModalDetail(false);
+    setEdit(false);
+    setEcheance({
+      numero: "",
+      redacteur: "Charles",
+      status: "",
+      category: "",
+      subject: "",
+      dateDebut: "",
+      dateFin: "",
+      lotId: {
+        id: "",
+        numero: "",
+        entreprise: {
+          nom: "",
+        },
+      },
+    });
+  };
+  const handleChangeEcheance = ({ currentTarget }) => {
+    const { name, value } = currentTarget;
+    setEcheance({ ...echeance, [name]: value });
+  };
+
+  const urlParams = match.params;
+  const reportById = fakeData.reportById(parseInt(urlParams.idReport, 10));
+  const fetchReport = () => {
+    setReport(reportById);
+  };
+
+  const handleEdit = () => {
+    setEdit(true);
+  };
+
+  const handleCloseEdit = () => {
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    fetchReport();
+  }, []);
+
+  console.log(reportById);
+  console.log(echeance);
+  return (
+    <>
+      <main>
+        <NavbarLeftWithRouter selected="echeances" />
+
+        <div className="page-content">
+          <h2 className="mb-4">Echéances : </h2>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Numéro</th>
+                <th>Rédacteur</th>
+                <th>Statut</th>
+                <th>Catégorie</th>
+                <th>Sujet</th>
+                <th>Début</th>
+                <th>Echéance</th>
+                <th>Clotûre</th>
+                <th>Retard</th>
+                <th>En charge</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {reportById.lots.map((lot) => (
+                <>
+                  {lot.echeances.map((echeance) => (
+                    <tr key={echeance.id}>
+                      <td>{echeance.id}</td>
+                      <td>{echeance.redacteur}</td>
+                      <td>
+                        <span
+                          className={
+                            "badge badge-" + STATUS_CLASSES[echeance.status]
+                          }
+                        >
+                          {STATUS_LABEL[echeance.status]}
+                        </span>
+                      </td>
+                      <td>{echeance.category}</td>
+                      <td>{echeance.subject}</td>
+                      <td>{DateAPI.formatDate(echeance.dateDebut)}</td>
+                      <td>{DateAPI.formatDate(echeance.dateFin)}</td>
+                      <td>{DateAPI.formatDate(echeance.dateCloture)}</td>
+                      <td>
+                        {DateAPI.retard(echeance.dateCloture, echeance.dateFin)}
+                      </td>
+                      <td>{lot.entreprise.nom}</td>
+                      <td>
+                        <Button
+                          className="btn btn-primary"
+                          text="Détails"
+                          onClick={() => handleShowModalDetail(echeance.id)}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </>
+              ))}
+            </tbody>
+          </table>
+          <Button
+            className="btn btn-primary"
+            text="Ajouter une échéance"
+            onClick={() => handleShowModalEcheance()}
+          />
+        </div>
+      </main>
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showModalEcheance}
+        onHide={handleCloseModalEcheance}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Ajouter une échéance</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <Field
+              name="category"
+              label="Catégorie"
+              onChange={handleChangeEcheance}
+              value={echeance.category}
+            ></Field>
+            <Field
+              name="subject"
+              label="Sujet"
+              onChange={handleChangeEcheance}
+              value={echeance.subject}
+            ></Field>
+            <Field
+              name="dateDebut"
+              label="Date de début"
+              type="date"
+              onChange={handleChangeEcheance}
+              value={echeance.dateDebut}
+            ></Field>
+            <Field
+              name="dateFin"
+              label="Date de fin prévue"
+              type="date"
+              onChange={handleChangeEcheance}
+              value={DateAPI.formatDateForm(echeance.dateFin)}
+            ></Field>
+            <Select
+              name="lot"
+              onChange={handleChangeEcheance}
+              value={echeance.lot}
+            >
+              <option>Selectionner un lot</option>
+              <option value="Apple">Lot N° 7548 Peinture</option>
+              <option value="IBM">Lot N° 4568 Moquette</option>
+              <option value="Sony">Lot N° 3154 Plomberie</option>
+            </Select>
+            <Modal.Footer>
+              <Button className="btn btn-primary" text="Ajouter"></Button>
+            </Modal.Footer>
+          </form>
+        </Modal.Body>
+      </Modal>
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showModalDetail}
+        onHide={handleCloseModalDetail}
+      >
+        <Modal.Header>
+          <h2>Detail de l'échéance N° {echeance.id}</h2>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="container d-flex  flex-wrap justify-content-around">
+              <div className="col-12 d-flex justify-content-around border-detail">
+                <p>Redacteur: {echeance.redacteur} </p>
+                <p>N° Lot: {echeance.lotId.numero} </p>
+              </div>
+              <div className="col-5 mt-3 border-detail d-flex flex-column justify-content-center">
+                <p>Categorie: {echeance.category} </p>
+                <p>Sujet: {echeance.subject} </p>
+                {edit ? (
+                  <Select
+                    name="status"
+                    label="Statut:"
+                    onChange={handleChangeEcheance}
+                    value={echeance.status}
+                  >
+                    <option value="no_start">Pas démarré</option>
+                    <option value="in_progress">En cours</option>
+                    <option value="late">En retard</option>
+                    <option value="finished">Fini</option>
+                  </Select>
+                ) : (
+                  <p>Statut: {STATUS_LABEL[echeance.status]}</p>
+                )}
+              </div>
+              <div className="col-5 mt-3 border-detail">
+                <p className="mt-3">Debut: {echeance.dateDebut} </p>
+                <p>Fin prévue: {echeance.dateFin} </p>
+                {edit ? (
+                  <Field
+                    name="dateCloture"
+                    label="Date de Cloture:"
+                    type="date"
+                    onChange={handleChangeEcheance}
+                    value={DateAPI.formatDateForm(echeance.dateCloture)}
+                  ></Field>
+                ) : (
+                  <p>Fini le: {echeance.dateCloture} </p>
+                )}
+                {DateAPI.retard(echeance.dateCloture, echeance.dateFin) > 0 && (
+                  <p>
+                    Retard:{" "}
+                    {DateAPI.retard(echeance.dateCloture, echeance.dateFin)}
+                  </p>
+                )}
+              </div>
+              <div className="col-12 border-detail mt-3">
+                <p className="mt-3">
+                  Entreprise en charge: {echeance.lotId.entreprise.nom}{" "}
+                </p>
+                <p>Effectif prévu: {echeance.effectifPrevu}</p>
+                {edit ? (
+                  <Field
+                    name="effectifConstate"
+                    label="Effectif constaté"
+                    type="text"
+                    onChange={handleChangeEcheance}
+                    value={echeance.effectifConstate}
+                  ></Field>
+                ) : (
+                  <p>Effectif constaté: {echeance.effectifConstate}</p>
+                )}
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          {!edit ? (
+            <Button
+              className="btn btn-primary"
+              text="Modifier"
+              onClick={handleEdit}
+            ></Button>
+          ) : (
+            <>
+              <Button
+                className="btn btn-danger"
+                text="Annuler"
+                onClick={handleCloseEdit}
+              ></Button>
+              <Button
+                className="btn btn-success"
+                text="Valider"
+                onClick={handleEdit}
+              ></Button>
+            </>
+          )}
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
--- assets/js/services/DateAPI.js
+++ assets/js/services/DateAPI.js
@@ -1,87 +1,89 @@
-
-import moment from "moment" ;
-
+import moment from "moment";
 
 function determineStatus(dateDebut, dateFinReelle) {
-    let status = "";
+  let status = "";
 
-    if (dateFinReelle === "") {
-        if (moment().isBefore(dateDebut)) {
-            status = "no_start";
-        } else {
-            status = "in_progress";
-        }
+  if (dateFinReelle === "") {
+    if (moment().isBefore(dateDebut)) {
+      status = "no_start";
     } else {
-        if (moment().diff(dateFinReelle, 'days') > 7) {
-            status = "archived";
-        } else {
-            status = "finished";
-        }
+      status = "in_progress";
     }
-    return status;
+  } else {
+    if (moment().diff(dateFinReelle, "days") > 7) {
+      status = "archived";
+    } else {
+      status = "finished";
+    }
+  }
+  return status;
 }
 
 function formatDate(date) {
-    return moment(date).format('DD/MM/YYYY');
+  if(date!==""){
+    return moment(date).format("DD/MM/YYYY");
+  }
 }
 
 function formatDateForm(date) {
-    return moment(date).format('YYYY-MM-DD');
+  return moment(date).format("YYYY-MM-DD");
 }
 
 function formatDateHours(date) {
-    return moment(date).format('DD/MM/YYYY à h:mm:ss');
+  return moment(date).format("DD/MM/YYYY à h:mm:ss");
 }
 
 function now() {
-    return moment();
+  return moment();
 }
 
 function verifyDateExist(date) {
-    if (moment(date).isSame("1900-01-01T00:00:00+00:00")) {
-        return "";
-    } else {
-        return date;
-    }
+  if (moment(date).isSame("1900-01-01T00:00:00+00:00")) {
+    return "";
+  } else {
+    return date;
+  }
 }
 
 function dateIsAfter(dateToCompare, dateDebut, dateFinPrevues) {
-    if (!dateIsAfterDebut(dateToCompare, dateDebut)) {
-        return false;
+  if (!dateIsAfterDebut(dateToCompare, dateDebut)) {
+    return false;
+  }
+
+  let higherDate = "1900-01-01T00:00:00+00:00";
+
+  dateFinPrevues.map((date) => {
+    if (moment(higherDate).isSameOrBefore(date.date)) {
+      higherDate = date.date;
+      console.log(higherDate);
+      console.log(date.date);
     }
+  });
+  console.log(higherDate);
 
-    let higherDate = "1900-01-01T00:00:00+00:00";
-
-    dateFinPrevues.map(
-        date => {
-            if (moment(higherDate).isSameOrBefore(date.date))
-            {
-                higherDate = date.date;
-                console.log(higherDate);
-                console.log(date.date);
-            }
-        }
-    )
-    console.log(higherDate);
-
-    return !moment(dateToCompare).isSameOrBefore(higherDate);
+  return !moment(dateToCompare).isSameOrBefore(higherDate);
 }
 
-
-function dateIsAfterDebut(dateToCompare, dateDebut)
-{
-    return !moment(dateToCompare).isSameOrBefore(dateDebut);
+function dateIsAfterDebut(dateToCompare, dateDebut) {
+  return !moment(dateToCompare).isSameOrBefore(dateDebut);
 }
 
+function retard(dateFin, dateDebut) {
+  if (dateFin !== "") {
+    return moment(dateFin).diff(moment(dateDebut), "days");
+  } else if (now().diff(moment(dateDebut), "days") > 0) {
+    return now().diff(moment(dateDebut), "days");
+  }
+}
 
 export default {
-    determineStatus,
-    formatDate,
-    formatDateHours,
-    verifyDateExist,
-    formatDateForm,
-    dateIsAfter,
-    dateIsAfterDebut,
-    now
-}
-
+  determineStatus,
+  formatDate,
+  formatDateHours,
+  verifyDateExist,
+  formatDateForm,
+  dateIsAfter,
+  dateIsAfterDebut,
+  now,
+  retard,
+};
